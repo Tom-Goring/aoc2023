757289adfb4021e56fdb965cb1ee21a44bdbe8f8
Tom Goring <mail@tomgoring.co.uk>
2023-12-02 14:54:43 +0000

    Day 2 p1

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
 #![feature(try_blocks)]
 
+use strum_macros::EnumString;
+use std::str::FromStr;
+
+use itertools::Itertools;
+
+#[derive(Debug, EnumString)]
+#[strum(ascii_case_insensitive)]
+enum Color {
+    Green,
+    Blue,
+    Red
+}
+
 fn main() {
-    let file = std::fs::read_to_string("day1.txt").unwrap();
+    let file = std::fs::read_to_string("day2.txt").unwrap();
 
     let total = file.lines().map(|line| {
-        // well this is horrifying
-        let line = line.replace("one", "o1e").replace("two", "t2o").replace("three", "t3e").replace("four", "f4r").replace("five", "f5e").replace("six", "s6x").replace("seven", "s7n").replace("eight", "e8t").replace("nine", "n9e");
+        let mut chars = line.chars();
 
-        let first = line.chars().filter(|c| c.is_numeric()).next();
-        let last = line.chars().rev().filter(|c| c.is_numeric()).next();
+        let id = chars.by_ref().skip_while(|c| c != &' ').skip(1).take_while(|c| c.is_numeric()).collect::<String>().parse::<usize>().unwrap();
 
-        (first, last)
-    }).map(|(first, last)| {
-        let x: Option<String> = try {
-            let first = first?;
-            let second = last?;
+        let impossible = chars
+            .skip(1)
+            .collect::<String>()
+            .split(";")
+            .map(str::trim)
+            .map(str::to_owned)
+            .map(|game| game.split(',').map(str::trim).map(str::to_owned).map(|game| game.split(' ').map(str::to_owned).collect_vec()).map(|array| (array[0].clone().parse::<usize>().unwrap(), Color::from_str(&array[1]).unwrap())).find(|game| match game.1 {
+                Color::Green => game.0 > 13,
+                Color::Blue => game.0 > 14,
+                Color::Red => game.0 > 12,
+            })).any(|game| game.is_some());
 
-            format!("{}{}", first, second)
-        };
+        // dbg!(&id, &impossible);
 
-        x
-    })
-    .filter_map(|str| str.map(|str| str.parse::<u32>()))
-    .sum::<Result<u32, _>>();
-    
-    println!("{total:?}");
+        (id, impossible)
+    }).filter(|(_, impossible)| !*impossible).map(|(id, _)| id).sum::<usize>();
+
+    dbg!(total);
 }
